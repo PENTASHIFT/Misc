4cf0dc3fda14c6bfeb5f365d47fa85e8840b2178
Joshua <void.query@gmail.com>
2017-04-30 00:36:28 -0700

    Add files via upload

---
+++ pseudo-chan.py
@@ -0,0 +1,45 @@
+from random import randint
+
+"""
+A script to roll a series of nine numbers like on *chan. Written in Python 3.
+Date Created: March 4th, 2017
+Edit: March 20th, 2017
+
+"""
+
+
+# How many rolls would you like?
+print ("Please enter how many rolls you'd like.")
+
+while True:
+   roll_num = input('> ')
+   try:
+      roll_num = int(roll_num)
+      break
+   except ValueError:
+      print ("Please enter a whole number.")
+      continue
+    
+
+# Checking the last digits of rolls for repeating numbers. 
+def checkRoll(roll):
+    if (roll[-2] != roll[-1]):
+        pass
+    else:
+        if (roll[-3] != roll[-1]):
+            roll += ' Dubs'
+        else:
+             roll += ' Trips+'
+    return roll
+
+# Gets a set of nine random numbers for the roll.
+def pseudoRoll():
+    outpt = "\t- "
+    for itr in range(9):
+        rand = randint(0, 9)
+        outpt += str(rand)
+    return outpt
+
+
+for _ in range(roll_num):
+    print (checkRoll(pseudoRoll()))
