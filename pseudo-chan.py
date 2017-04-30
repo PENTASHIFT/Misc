41090a8cf06d21586a717fc8f9e4446682210b23
Joshua <void.query@gmail.com>
2017-04-30 00:38:26 -0700

    Update pseudo-chan.py

--- pseudo-chan.py
+++ pseudo-chan.py
@@ -1,7 +1,9 @@
 from random import randint
 
 """
-A script to roll a series of nine numbers like on *chan. Written in Python 3.
+A script to roll a series of nine numbers like on *chan. Written in Python 3. 
+Complete with fairly unhelpful commenting.
+
 Date Created: March 4th, 2017
 Edit: March 20th, 2017
 
@@ -9,7 +11,7 @@ Edit: March 20th, 2017
 
 
 # How many rolls would you like?
-print ("Please enter how many rolls you'd like.")
+print("Please enter how many rolls you'd like.")
 
 while True:
    roll_num = input('> ')
@@ -17,16 +19,16 @@ while True:
       roll_num = int(roll_num)
       break
    except ValueError:
-      print ("Please enter a whole number.")
+      print("Please enter a whole number.")
       continue
     
 
 # Checking the last digits of rolls for repeating numbers. 
 def checkRoll(roll):
-    if (roll[-2] != roll[-1]):
+    if(roll[-2] != roll[-1]):
         pass
     else:
-        if (roll[-3] != roll[-1]):
+        if(roll[-3] != roll[-1]):
             roll += ' Dubs'
         else:
              roll += ' Trips+'
@@ -42,4 +44,4 @@ def pseudoRoll():
 
 
 for _ in range(roll_num):
-    print (checkRoll(pseudoRoll()))
+    print(checkRoll(pseudoRoll()))
