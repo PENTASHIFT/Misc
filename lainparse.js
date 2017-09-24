0d680b51b9ab1145edc50bfd62f22957ad8d2425
Joshua Munsey <void.query@gmail.com>
2017-09-24 13:16:52 -0700

    A web extension to de-censor lainchan.

--- lainparse.js
+++ lainparse.js
@@ -1,10 +1,11 @@
 // An extension to filter through the censoring nonsense on lainchan.
 
+// The class name of each post.
 var elements = document.getElementsByClassName("body");
 
-var i;
-for(i=0; i<elements.length; i++){
-	var content = elements[i].textContent;
-	var parsed = content.replace(/fuarrrk/g, "fuck");
+// Looping through each post and filtering.
+for(var i=0; i<elements.length; i++){
+	let content = elements[i].textContent;
+	let parsed = content.replace(/fuarrrk/g, "fuck");
 	elements[i].innerHTML = parsed;
 }
