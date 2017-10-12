762071d9976d64f67a04a54e30c1e3f911e493b5
Joshua Munsey <void.query@gmail.com>
2017-10-11 21:17:24 -0700

    Updated to be readable and nicer.

--- back-up.py
+++ back-up.py
@@ -1,23 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """ An automated, functional backup script for *nix based systems.
-    Utilizing parallel functionality to refrain from wasted time. """
+    Utilizing parallel functionality to refrain from wasted time.
+    Meant to have a udev rule applied to automatically run when
+    an external drive is plugged in. """
 
 import os
-import sys
 from shutil import copy
 from threading import Thread
 
-def backup(x):
-    pass
+def identify(item, cwd):
+    """ Recieves a single item from the tuple from BACKUP and checks
+        whether it's a directory, if not it backs it up. """`
+    cwd = os.path.join(cwd, item)
+    if os.path.isdir(cwd):
+        print('Parsing: %s.' %item)
+        parse_dir(cwd)
+    else:
+        print('Copying: %s' %item)
+        copy(cwd, end)
 
-def parse_dir(x):
+def backup(lst, cwd):
+    """ Recieves half the tuple from PARSE_DIR and the directory then 
+        maps the tuple to the identify function. """
+    list(map(lambda x: identify(x, cwd), lst))
+
+def parse_dir(cwd):
     """ Recieves the directory and then parses the contents 
         into a two item tuple to then backup in parallel. """
-    y, z = divmod(len(x), 2)
-    return (x[:y + z], x[y + z:])
-    
+    lst = os.listdir(cwd)
+    if not len(lst) <= 1:
+        y, z = divmod(len(lst), 2)
+        lst = (lst[:y + z], lst[y + z:])
+        t1 = Thread(target=backup, args=(lst[0], cwd,))
+        t2 = Thread(target=backup, args=(lst[1], cwd,))
+        t1.start()
+        t2.start()
+    else:
+        backup(lst, cwd)
 
-# PSEUDO-CODE
-x = os.listdir('/home/gary/Pictures/')    
-print(os.path.isdir(parse_dir(x[0])[0]))
+# Meta Code.
+start = '<directory to backup>'
+end = '<ending directory here>'
+parse_dir(start)
